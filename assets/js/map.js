639d95db56b5d36c1084161348c292f85d672acf
Darrin Fong <darrinfong@gmail.com>
2015-10-29 01:24:45 -0400

    Map re-center

--- assets/js/map.js
+++ assets/js/map.js
@@ -5,7 +5,7 @@
 function initMap() {
   var map = new google.maps.Map(document.getElementById('map'), {
     zoom: 10,
-    center: {lat: -33.9, lng: 151.2}
+    center: {lat: 45.510903, lng: 73.57615}
   });
 
   setMarkers(map);
